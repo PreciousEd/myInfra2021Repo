649b867604bceda4e035d399a2dee7cbebd33ec3
PreciousEd <160648013+PreciousEd@users.noreply.github.com>
2024-04-10 20:32:30 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state-2026"
+    bucket = "my-s3-bucket-for-tf-state-2027"
     key = "main"
-    region = "us-east-2"
+    region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
